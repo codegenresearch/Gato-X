f4f4696de53ae89a7126139a26dc12cf56b85b4b
venkatkrishna <venkatkrishna@moe>
2025-02-09 17:43:59 +0100

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/organization.py
+++ gatox/models/organization.py
@@ -1,28 +1,29 @@
+from typing import List
 from gatox.models.runner import Runner
 from gatox.models.repository import Repository
 from gatox.models.secret import Secret
 
 
-class Organization():
+class Organization:
 
     def __init__(self, org_data: dict, user_scopes: list, limited_data: bool = False):
         """Wrapper object for an organization.
 
         Args:
-            org_data (dict): Org data from GitHub API
-            user_scopes (list): List of OAuth scopes that the PAT has
-            limited_data (bool): Whether limited org_data is present (default: False)
+            org_data (dict): Org data from GitHub API.
+            user_scopes (list): List of OAuth scopes that the PAT has.
+            limited_data (bool): Whether limited org_data is present (default: False).
         """
         self.name = None
         self.org_admin_user = False
         self.org_admin_scopes = False
         self.org_member = False
-        self.secrets: list[Secret] = []
-        self.runners: list[Runner] = []
+        self.secrets: List[Secret] = []
+        self.runners: List[Runner] = []
         self.sso_enabled = False
         self.limited_data = limited_data
-        self.public_repos = []
-        self.private_repos = []
+        self.public_repos: List[Repository] = []
+        self.private_repos: List[Repository] = []
 
         self.name = org_data['login']
 
@@ -32,52 +33,47 @@ class Organization():
             if "admin:org" in user_scopes:
                 self.org_admin_scopes = True
                 self.org_admin_user = True
-        elif "billing_email" in org_data:
-            self.org_member = True
-        # If billing_email is not present, the user is neither an admin nor a member
-        else:
-            self.org_member = False
 
-    def set_secrets(self, secrets: list[Secret]):
+    def set_secrets(self, secrets: List[Secret]):
         """Set repo-level secrets.
 
         Args:
-            secrets (list[Secret]): List of secrets at the organization level.
+            secrets (List[Secret]): List of secrets at the organization level.
         """
         self.secrets = secrets
 
     def set_repository(self, repo: Repository):
-        """Add a single repository object to the organization.
+        """Add a single repository to the organization.
 
         Args:
-            repo (Repository): Repository wrapper object to add.
+            repo (Repository): Repository to add.
         """
         if repo.is_private():
             self.private_repos.append(repo)
         else:
             self.public_repos.append(repo)
 
-    def set_public_repos(self, repos: list[Repository]):
-        """Set the list of public repository objects for the org.
+    def set_public_repos(self, repos: List[Repository]):
+        """Set the list of public repositories for the org.
 
         Args:
-            repos (list[Repository]): List of Repository wrapper objects.
+            repos (List[Repository]): List of Repository wrapper objects.
         """
         self.public_repos = repos
 
-    def set_private_repos(self, repos: list[Repository]):
-        """Set the list of private repository objects for the org.
+    def set_private_repos(self, repos: List[Repository]):
+        """Set the list of private repositories for the org.
 
         Args:
-            repos (list[Repository]): List of Repository wrapper objects.
+            repos (List[Repository]): List of Repository wrapper objects.
         """
         self.private_repos = repos
 
-    def set_runners(self, runners: list[Runner]):
+    def set_runners(self, runners: List[Runner]):
         """Set a list of runners that the organization can access.
 
         Args:
-            runners (list[Runner]): List of runners that are attached to the organization.
+            runners (List[Runner]): List of runners that are attached to the organization.
         """
         self.runners = runners
 
@@ -114,12 +110,8 @@ class Organization():
                 "org_runners": [runner.toJSON() for runner in self.runners],
                 "org_secrets": [secret.toJSON() for secret in self.secrets],
                 "sso_access": self.sso_enabled,
-                "public_repos": [
-                    repository.toJSON() for repository in self.public_repos
-                ],
-                "private_repos": [
-                    repository.toJSON() for repository in self.private_repos
-                ]
+                "public_repos": [repository.toJSON() for repository in self.public_repos],
+                "private_repos": [repository.toJSON() for repository in self.private_repos]
             }
 
         return representation

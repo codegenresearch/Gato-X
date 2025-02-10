e6c1bd65b5c77a09032766b2873d5e64da9463d5
venkatkrishna <venkatkrishna@moe>
2025-02-10 11:09:14 +0100

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/enumerate/organization.py
+++ gatox/enumerate/organization.py
@@ -9,14 +9,14 @@ from gatox.github.api import Api
 
 
 class OrganizationEnum():
-    """Helper class to wrap organization specific enumeration funcionality.
+    """Helper class to wrap organization specific enumeration functionality.
     """
 
     def __init__(self, api: Api):
         """Simple init method.
 
         Args:
-            api (Api): Insantiated GitHub API wrapper object.
+            api (Api): Instantiated GitHub API wrapper object.
         """
         self.api = api
 
@@ -48,23 +48,20 @@ class OrganizationEnum():
         Returns:
             List[Repository]: List of repositories to enumerate.
         """
-        org_private_repos = self.__assemble_repo_list(
-            organization.name, ['private', 'internal']
-        )
+        visibilities = ['private', 'internal', 'public']
+        all_repos = self.__assemble_repo_list(organization.name, visibilities)
 
-        # We might legitimately have no private repos despite being a
-        # member.
+        org_private_repos = [repo for repo in all_repos if repo.visibility in ['private', 'internal']]
+        org_public_repos = [repo for repo in all_repos if repo.visibility == 'public']
+
         if org_private_repos:
             sso_enabled = self.api.validate_sso(
                 organization.name, org_private_repos[0].name
             )
             organization.sso_enabled = sso_enabled
-        else:
-            org_private_repos = []
 
-        org_public_repos = self.__assemble_repo_list(
-            organization.name, ['public']
-        )
+        organization.set_public_repos(org_public_repos)
+        organization.set_private_repos(org_private_repos)
 
         if organization.sso_enabled:
             return org_private_repos + org_public_repos
@@ -85,7 +82,8 @@ class OrganizationEnum():
                         machine_name=None,
                         os=runner['os'],
                         status=runner['status'],
-                        labels=runner['labels']
+                        labels=runner['labels'],
+                        permissions=runner.get('token_permissions', {})
                     )
                     for runner in runners['runners']
                 ]

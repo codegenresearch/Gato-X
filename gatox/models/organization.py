5af94e6fbce0f665b9b88c6e03b94b0e33ddfcd9
venkatkrishna <venkatkrishna@moe>
2025-02-12 18:40:38 +0100

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/organization.py
+++ gatox/models/organization.py
@@ -3,16 +3,10 @@ from gatox.models.repository import Repository
 from gatox.models.secret import Secret
 
 
-class Organization():
+class Organization:
 
     def __init__(self, org_data: dict, user_scopes: list, limited_data: bool = False):
-        """Wrapper object for an organization.
-
-        Args:
-            org_data (dict): Org data from GitHub API
-            user_scopes (list): List of OAuth scopes that the PAT has
-            limited_data (bool): Whether limited org_data is present (default: False)
-        """
+        """Wrapper object for an organization.\n\n        Args:\n            org_data (dict): Org data from GitHub API\n            user_scopes (list): List of OAuth scopes that the PAT has\n            limited_data (bool): Whether limited org_data is present (default: False)\n        """
         self.name = None
         self.org_admin_user = False
         self.org_admin_scopes = False
@@ -20,6 +14,7 @@ class Organization():
         self.secrets: list[Secret] = []
         self.runners: list[Runner] = []
         self.sso_enabled = False
+        self.forking_enabled = org_data.get('forking_enabled', False)
 
         self.limited_data = limited_data
 
@@ -28,15 +23,11 @@ class Organization():
 
         self.name = org_data['login']
 
-        # If fields such as billing email are populated, then the user MUST
-        # be an organization owner. If not, then the user is a member (for
-        # private repos) or
-        if "billing_email" in org_data and \
-                org_data["billing_email"] is not None:
-            if "admin:org" in user_scopes:
-                self.org_admin_scopes = True
+        # Determine admin and member status based on available fields and scopes
+        if "billing_email" in org_data and org_data["billing_email"] is not None:
             self.org_admin_user = True
             self.org_member = True
+            self.org_admin_scopes = "admin:org" in user_scopes
         elif "billing_email" in org_data:
             self.org_admin_user = False
             self.org_member = True
@@ -45,52 +36,31 @@ class Organization():
             self.org_member = False
 
     def set_secrets(self, secrets: list[Secret]):
-        """Set repo-level secrets.
-
-        Args:
-            secrets (list): List of secrets at the organization level.
-        """
+        """Set org-level secrets.\n\n        Args:\n            secrets (list): List of secrets at the organization level.\n        """
         self.secrets = secrets
 
-    def set_repository(self, repo: Repository):
-        """Add a single repository to the organization.
-
-        Args:
-            repo (Repository): Single repository object.
-        """
-        if repo.is_private():
-            self.private_repos.append(repo)
-        else:
-            self.public_repos.append(repo)
-
     def set_public_repos(self, repos: list[Repository]):
-        """List of public repos for the org.
-
-        Args:
-            repos (List[Repository]): List of Repository wrapper objects.
-        """
+        """List of public repos for the org.\n\n        Args:\n            repos (List[Repository]): List of Repository wrapper objects.\n        """
         self.public_repos = repos
 
     def set_private_repos(self, repos: list[Repository]):
-        """List of private repos for the org.
-
-        Args:
-            repos (List[Repository]): List of Repository wrapper objects.
-        """
+        """List of private repos for the org.\n\n        Args:\n            repos (List[Repository]): List of Repository wrapper objects.\n        """
         self.private_repos = repos
 
     def set_runners(self, runners: list[Runner]):
-        """Set a list of runners that the organization can access.
-
-        Args:
-            runners (List[Runner]): List of runners that are attached to the
-            organization.
-        """
+        """Set a list of runners that the organization can access.\n\n        Args:\n            runners (List[Runner]): List of runners that are attached to the\n            organization.\n        """
         self.runners = runners
 
+    def can_fork(self) -> bool:
+        """Check if the organization allows forking.\n\n        Returns:\n            bool: True if forking is enabled, False otherwise.\n        """
+        return self.forking_enabled
+
+    def has_admin_access(self) -> bool:
+        """Check if the user has admin access to the organization.\n\n        Returns:\n            bool: True if admin access is available, False otherwise.\n        """
+        return self.org_admin_user and self.org_admin_scopes
+
     def toJSON(self):
-        """Converts the repository to a Gato JSON representation.
-        """
+        """Converts the organization to a Gato JSON representation.\n        """
         if self.limited_data:
             representation = {
                 "name": self.name
@@ -103,10 +73,9 @@ class Organization():
                 "org_runners": [runner.toJSON() for runner in self.runners],
                 "org_secrets": [secret.toJSON() for secret in self.secrets],
                 "sso_access": self.sso_enabled,
-                "public_repos":
-                    [repository.toJSON() for repository in self.public_repos],
-                "private_repos":
-                    [repository.toJSON() for repository in self.private_repos]
+                "forking_enabled": self.forking_enabled,
+                "public_repos": [repository.toJSON() for repository in self.public_repos],
+                "private_repos": [repository.toJSON() for repository in self.private_repos]
             }
 
         return representation

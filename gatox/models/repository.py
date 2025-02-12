acb1687f68c3fc88f7dc6a6433d5539560b46a19
venkatkrishna <venkatkrishna@moe>
2025-02-12 13:18:25 +0100

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/repository.py
+++ gatox/models/repository.py
@@ -5,17 +5,10 @@ from gatox.models.secret import Secret
 
 
 class Repository():
-    """Simple wrapper class to provide accessor methods against the repository
-    JSON response from GitHub.
-    """
+    """Simple wrapper class to provide accessor methods against the repository\n    JSON response from GitHub.\n    """
 
-    def __init__(self, repo_data: dict):
-        """Initialize wrapper class.
-
-        Args:
-            repo_json (dict): Dictionary from parsing JSON object returned from
-            GitHub
-        """
+    def __init__(self, repo_data: dict, user_permissions: dict):
+        """Initialize wrapper class.\n\n        Args:\n            repo_json (dict): Dictionary from parsing JSON object returned from\n            GitHub\n            user_permissions (dict): Dictionary containing user permissions for the repository\n        """
         self.repo_data = repo_data
         # Temporary hack until full transition to GQL
         if 'environments' not in self.repo_data:
@@ -28,7 +21,7 @@ class Repository():
         self.sh_workflow_names = []
         self.enum_time = datetime.datetime.now()
 
-        self.permission_data = self.repo_data['permissions']
+        self.permission_data = user_permissions
         self.sh_runner_access = False
         self.accessible_runners: list[Runner] = []
         self.runners: list[Runner] = []
@@ -48,7 +41,7 @@ class Repository():
         return self.permission_data.get('pull', False)
 
     def is_private(self):
-        return self.repo_data['visibility'] != 'public'
+        return self.repo_data['private']
     
     def is_archived(self):
         return self.repo_data['archived']
@@ -69,31 +62,23 @@ class Repository():
         return f"{self.repo_data['html_url']}/blob/{self.repo_data['default_branch']}"
 
     def update_time(self):
-        """Update timestamp.
-        """
+        """Update timestamp.\n        """
         self.enum_time = datetime.datetime.now()
 
     def set_accessible_org_secrets(self, secrets: list[Secret]):
-        """Sets organization secrets that can be read using a workflow in
-        this repository.
-
-        Args:
-            secrets (List[Secret]): List of Secret wrapper objects.
-        """
+        """Sets organization secrets that can be read using a workflow in\n        this repository.\n\n        Args:\n            secrets (List[Secret]): List of Secret wrapper objects.\n        """
         self.org_secrets = secrets
 
     def set_pwn_request(self, pwn_request_package: dict):
         self.pwn_req_risk.append(pwn_request_package)
 
     def clear_pwn_request(self, workflow_name):
-        """Remove pwn request entry since it's a false positive.
-        """
-        self.pwn_req_risk = [element for element in self.pwn_req_risk if \
+        """Remove pwn request entry since it's a false positive.\n        """
+        self.pwn_req_risk = [element for element in self.pwn_req_risk if 
                              element['workflow_name'] != workflow_name]
 
     def has_pwn_request(self):
-        """Return True if there are any pwn request risks.
-        """
+        """Return True if there are any pwn request risks.\n        """
         return len(self.pwn_req_risk) > 0
 
     def set_injection(self, injection_package: dict):
@@ -101,51 +86,37 @@ class Repository():
         self.injection_risk.append(injection_package)
 
     def has_injection(self):
-        """Return True if there are any injection risks.
-        """
+        """Return True if there are any injection risks.\n        """
         return len(self.injection_risk) > 0
 
     def set_secrets(self, secrets: list[Secret]):
-        """Sets secrets that are attached to this repository.
-
-        Args:
-            secrets (List[Secret]): List of repo level secret wrapper objects.
-        """
+        """Sets secrets that are attached to this repository.\n\n        Args:\n            secrets (List[Secret]): List of repo level secret wrapper objects.\n        """
         self.secrets = secrets
 
     def set_runners(self, runners: list[Runner]):
-        """Sets list of self-hosted runners attached at the repository level.
-        """
+        """Sets list of self-hosted runners attached at the repository level.\n        """
         self.sh_runner_access = True
         self.runners = runners
 
     def add_self_hosted_workflows(self, workflows: list):
-        """Add a list of workflow file names that run on self-hosted runners.
-        """
+        """Add a list of workflow file names that run on self-hosted runners.\n        """
         self.sh_workflow_names.extend(workflows)
 
     def add_accessible_runner(self, runner: Runner):
-        """Add a runner is accessible by this repo. This runner could be org
-        level or repo level.
-
-        Args:
-            runner (Runner): Runner wrapper object
-        """
+        """Add a runner is accessible by this repo. This runner could be org\n        level or repo level.\n\n        Args:\n            runner (Runner): Runner wrapper object\n        """
         self.sh_runner_access = True
         self.accessible_runners.append(runner)
 
     def toJSON(self):
-        """Converts the repository to a Gato JSON representation.
-        """
+        """Converts the repository to a Gato JSON representation.\n        """
         representation = {
             "name": self.name,
-            "enum_time": self.enum_time.ctime(),
+            "enum_time": self.enum_time.isoformat(),
             "permissions": self.permission_data,
             "can_fork": self.can_fork(),
             "stars": self.repo_data['stargazers_count'],
-            "runner_workflows": [wf for wf in self.sh_workflow_names],
-            "accessible_runners": [runner.toJSON() for runner
-                                   in self.accessible_runners],
+            "runner_workflows": self.sh_workflow_names,
+            "accessible_runners": [runner.toJSON() for runner in self.accessible_runners],
             "repo_runners": [runner.toJSON() for runner in self.runners],
             "repo_secrets": [secret.toJSON() for secret in self.secrets],
             "org_secrets": [secret.toJSON() for secret in self.org_secrets],

f5df85f8408903bbde1149e33c297bb91d95f1be
venkatkrishna <venkatkrishna@moe>
2025-02-12 17:46:25 +0100

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/organization.py
+++ gatox/models/organization.py
@@ -3,110 +3,75 @@ from gatox.models.repository import Repository
 from gatox.models.secret import Secret
 
 
-class Organization():
+class Organization:
 
     def __init__(self, org_data: dict, user_scopes: list, limited_data: bool = False):
-        """Wrapper object for an organization.
-
-        Args:
-            org_data (dict): Org data from GitHub API
-            user_scopes (list): List of OAuth scopes that the PAT has
-            limited_data (bool): Whether limited org_data is present (default: False)
-        """
-        self.name = None
+        """Wrapper object for an organization.\n\n        Args:\n            org_data (dict): Org data from GitHub API\n            user_scopes (list): List of OAuth scopes that the PAT has\n            limited_data (bool): Whether limited org_data is present (default: False)\n        """
+        self.name = org_data.get('login')
         self.org_admin_user = False
         self.org_admin_scopes = False
         self.org_member = False
         self.secrets: list[Secret] = []
         self.runners: list[Runner] = []
         self.sso_enabled = False
-
         self.limited_data = limited_data
-
         self.public_repos = []
         self.private_repos = []
 
-        self.name = org_data['login']
+        self._determine_user_role(org_data, user_scopes)
 
-        # If fields such as billing email are populated, then the user MUST
-        # be an organization owner. If not, then the user is a member (for
-        # private repos) or
-        if "billing_email" in org_data and \
-                org_data["billing_email"] is not None:
+    def _determine_user_role(self, org_data: dict, user_scopes: list):
+        """Determine the user's role within the organization based on the provided data and scopes."""
+        if "billing_email" in org_data and org_data["billing_email"] is not None:
+            self.org_member = True
             if "admin:org" in user_scopes:
                 self.org_admin_scopes = True
-            self.org_admin_user = True
-            self.org_member = True
+                self.org_admin_user = True
         elif "billing_email" in org_data:
-            self.org_admin_user = False
             self.org_member = True
         else:
-            self.org_admin_user = False
             self.org_member = False
 
     def set_secrets(self, secrets: list[Secret]):
-        """Set repo-level secrets.
-
-        Args:
-            secrets (list): List of secrets at the organization level.
-        """
+        """Set organization-level secrets.\n\n        Args:\n            secrets (list[Secret]): List of secrets at the organization level.\n        """
         self.secrets = secrets
 
-    def set_repository(self, repo: Repository):
-        """Add a single repository to the organization.
-
-        Args:
-            repo (Repository): Single repository object.
-        """
-        if repo.is_private():
-            self.private_repos.append(repo)
-        else:
-            self.public_repos.append(repo)
-
     def set_public_repos(self, repos: list[Repository]):
-        """List of public repos for the org.
-
-        Args:
-            repos (List[Repository]): List of Repository wrapper objects.
-        """
+        """Set the list of public repositories for the organization.\n\n        Args:\n            repos (list[Repository]): List of Repository wrapper objects.\n        """
         self.public_repos = repos
 
     def set_private_repos(self, repos: list[Repository]):
-        """List of private repos for the org.
-
-        Args:
-            repos (List[Repository]): List of Repository wrapper objects.
-        """
+        """Set the list of private repositories for the organization.\n\n        Args:\n            repos (list[Repository]): List of Repository wrapper objects.\n        """
         self.private_repos = repos
 
     def set_runners(self, runners: list[Runner]):
-        """Set a list of runners that the organization can access.
-
-        Args:
-            runners (List[Runner]): List of runners that are attached to the
-            organization.
-        """
+        """Set the list of runners that the organization can access.\n\n        Args:\n            runners (list[Runner]): List of runners attached to the organization.\n        """
         self.runners = runners
 
-    def toJSON(self):
-        """Converts the repository to a Gato JSON representation.
-        """
-        if self.limited_data:
-            representation = {
-                "name": self.name
-            }
-        else:
-            representation = {
-                "name": self.name,
-                "org_admin_user": self.org_admin_user,
-                "org_member": self.org_member,
-                "org_runners": [runner.toJSON() for runner in self.runners],
-                "org_secrets": [secret.toJSON() for secret in self.secrets],
-                "sso_access": self.sso_enabled,
-                "public_repos":
-                    [repository.toJSON() for repository in self.public_repos],
-                "private_repos":
-                    [repository.toJSON() for repository in self.private_repos]
-            }
+    def has_admin_scopes(self) -> bool:
+        """Check if the user has admin scopes for the organization."""
+        return self.org_admin_scopes
 
-        return representation
+    def has_admin_role(self) -> bool:
+        """Check if the user has an admin role in the organization."""
+        return self.org_admin_user
+
+    def is_member(self) -> bool:
+        """Check if the user is a member of the organization."""
+        return self.org_member
+
+    def toJSON(self):
+        """Converts the organization to a Gato JSON representation."""
+        if self.limited_data:
+            return {"name": self.name}
+
+        return {
+            "name": self.name,
+            "org_admin_user": self.org_admin_user,
+            "org_member": self.org_member,
+            "org_runners": [runner.toJSON() for runner in self.runners],
+            "org_secrets": [secret.toJSON() for secret in self.secrets],
+            "sso_access": self.sso_enabled,
+            "public_repos": [repo.toJSON() for repo in self.public_repos],
+            "private_repos": [repo.toJSON() for repo in self.private_repos]
+        }

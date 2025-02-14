2c1efccf28c86c0745edca04752adf9ac93d0265
venkatkrishna <venkatkrishna@moe>
2025-02-14 12:19:01 +0100

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/github/gql_queries.py
+++ gatox/github/gql_queries.py
@@ -1,159 +1,25 @@
 class GqlQueries():
-    """Constructs graphql queries for use with the GitHub GraphQL api.
-    """
+    """Constructs graphql queries for use with the GitHub GraphQL api.\n    """
 
-    GET_YMLS_WITH_SLUGS = """
-    fragment repoWorkflows on Repository {
-        nameWithOwner
-        stargazers {
-            totalCount
-        }
-        isPrivate
-        isArchived
-        viewerPermission
-        forkingAllowed
-        url
-        isFork
-        pushedAt
-        defaultBranchRef {
-            name
-        }
-        object(expression: "HEAD:.github/workflows/") {
-            ... on Tree {
-                entries {
-                    name
-                    type
-                    mode
-                    object {
-                        ... on Blob {
-                            byteSize
-                            text
-                        }
-                    }
-                }
-            }
-        }
-    }
-    """
+    GET_YMLS_WITH_SLUGS = """\n    fragment repoWorkflows on Repository {\n        nameWithOwner\n        stargazers {\n            totalCount\n        }\n        isPrivate\n        isArchived\n        viewerPermission\n        url\n        isFork\n        pushedAt\n        defaultBranchRef {\n            name\n        }\n        object(expression: "HEAD:.github/workflows/") {\n            ... on Tree {\n                entries {\n                    name\n                    type\n                    mode\n                    object {\n                        ... on Blob {\n                            byteSize\n                            text\n                        }\n                    }\n                }\n            }\n        }\n    }\n    """
 
-    GET_YMLS = """
-    query RepoFiles($node_ids: [ID!]!) {
-        nodes(ids: $node_ids) {
-            ... on Repository {
-                nameWithOwner
-                isPrivate
-                isArchived
-                forkingAllowed
-                stargazers {
-                    totalCount
-                }
-                viewerPermission
-                pushedAt
-                url
-                isFork
-                defaultBranchRef {
-                    name
-                }
-                object(expression: "HEAD:.github/workflows/") {
-                    ... on Tree {
-                        entries {
-                            name
-                            type
-                            mode
-                            object {
-                                ... on Blob {
-                                    byteSize
-                                    text
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    """
+    GET_YMLS = """\n    query RepoFiles($node_ids: [ID!]!) {\n        nodes(ids: $node_ids) {\n            ... on Repository {\n                ...repoWorkflows\n            }\n        }\n    }\n    """
 
-    GET_YMLS_ENV = """
-        query RepoFiles($node_ids: [ID!]!) {
-            nodes(ids: $node_ids) {
-                ... on Repository {
-                    nameWithOwner
-                    isPrivate
-                    isArchived
-                    forkingAllowed
-                    stargazers {
-                        totalCount
-                    }
-                    viewerPermission
-                    pushedAt
-                    url
-                    isFork
-                    environments(first: 100) {
-                        edges {
-                        node {
-                            id
-                            name
-                        }
-                    }
-                    }
-                    defaultBranchRef {
-                        name
-                    }
-                    object(expression: "HEAD:.github/workflows/") {
-                        ... on Tree {
-                            entries {
-                                name
-                                type
-                                mode
-                                object {
-                                    ... on Blob {
-                                        byteSize
-                                        text
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    """
+    GET_YMLS_ENV = """\n    query RepoFiles($node_ids: [ID!]!) {\n        nodes(ids: $node_ids) {\n            ... on Repository {\n                ...repoWorkflows\n                environments(first: 100) {\n                    edges {\n                    node {\n                        id\n                        name\n                    }\n                    }\n                }\n            }\n        }\n    }\n    """
 
     @staticmethod
     def get_workflow_ymls_from_list(repos: list):
-        """
-        Constructs a list of GraphQL queries to fetch workflow YAML 
-        files from a list of repositories.
-
-        This method splits the list of repositories into chunks of 
-        up to 100 repositories each, and constructs a separate
-        GraphQL query for each chunk. Each query fetches the workflow 
-        YAML files from the repositories in one chunk.
-
-        Args:
-            repos (list): A list of repository slugs, where each 
-            slug is a string in the format "owner/name".
-
-        Returns:
-            list: A list of dictionaries, where each dictionary 
-            contains a single GraphQL query in the format:
-            {"query": "<GraphQL query string>"}.
-        """
+        """\n        Constructs a list of GraphQL queries to fetch workflow YAML \n        files from a list of repositories.\n\n        This method splits the list of repositories into chunks of \n        up to 100 repositories each, and constructs a separate\n        GraphQL query for each chunk. Each query fetches the workflow \n        YAML files from the repositories in one chunk.\n\n        Args:\n            repos (list): A list of repository slugs, where each \n            slug is a string in the format "owner/name".\n\n        Returns:\n            list: A list of dictionaries, where each dictionary \n            contains a single GraphQL query in the format:\n            {"query": "<GraphQL query string>"}.\n        """
         
         queries = []
 
-        for i in range(0, len(repos), 50):
-            chunk = repos[i:i + 50]
+        for i in range(0, len(repos), 100):
+            chunk = repos[i:i + 100]
             repo_queries = []
 
             for j, repo in enumerate(chunk):
                 owner, name = repo.split('/')
-                repo_query = f"""
-                repo{j + 1}: repository(owner: "{owner}", name: "{name}") {{
-                    ...repoWorkflows
-                }}
-                """
+                repo_query = f"""\n                repo{j + 1}: repository(owner: "{owner}", name: "{name}") {{\n                    ...repoWorkflows\n                }}\n                """
                 repo_queries.append(repo_query)
 
             queries.append(
@@ -164,28 +30,19 @@ class GqlQueries():
 
     @staticmethod
     def get_workflow_ymls(repos: list):
-        """Retrieve workflow yml files for each repository.
-
-        Args:
-            repos (List[Repository]): List of repository objects
-        Returns:
-            (list): List of JSON post parameters for each graphQL query.
-        """
+        """Retrieve workflow yml files for each repository.\n\n        Args:\n            repos (List[Repository]): List of repository objects\n        Returns:\n            (list): List of JSON post parameters for each graphQL query.\n        """
         queries = []
 
         if len(repos) == 0:
             return queries
 
-        for i in range(0, (len(repos) // 100) + 1):
-
-            top_len = len(repos) if len(repos) < (100 + i*100) else (100 + i*100)
+        for i in range(0, len(repos), 100):
+            chunk = repos[i:i + 100]
             query = {
-                # We list envs if we have write access to one in the set (for secrets
-                # reasons, otherwise we don't list them)
-                "query": GqlQueries.GET_YMLS_ENV if repos[i].can_push() else GqlQueries.GET_YMLS,
+                "query": GqlQueries.GET_YMLS_ENV if any(repo.can_push() for repo in chunk) else GqlQueries.GET_YMLS,
                 "variables": {
                     "node_ids": [
-                        repo.repo_data['node_id'] for repo in repos[0+100*i:top_len]
+                        repo.repo_data['node_id'] for repo in chunk
                     ]
                 }
             }

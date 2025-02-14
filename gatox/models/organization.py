81c9d6346077b382b6453c706fef19b9095c03ff
venkatkrishna <venkatkrishna@moe>
2025-02-14 12:19:01 +0100

    Reset to 332b947fc25228c9b5e8f892b1e8dc7c9f6c4408 and made changes for LLM Round 1

--- gatox/models/organization.py
+++ gatox/models/organization.py
@@ -1,3 +1,4 @@
+from typing import List
 from gatox.models.runner import Runner
 from gatox.models.repository import Repository
 from gatox.models.secret import Secret
@@ -6,25 +7,21 @@ from gatox.models.secret import Secret
 class Organization():
 
     def __init__(self, org_data: dict, user_scopes: list, limited_data: bool = False):
-        """Wrapper object for an organization.
-
-        Args:
-            org_data (dict): Org data from GitHub API
-            user_scopes (list): List of OAuth scopes that the PAT has
-            limited_data (bool): Whether limited org_data is present (default: False)
-        """
+        """Wrapper object for an organization.\n\n        Args:\n            org_data (dict): Org data from GitHub API\n            user_scopes (list): List of OAuth scopes that the PAT has\n            limited_data (bool): Whether limited org_data is present (default: False)\n        """
         self.name = None
         self.org_admin_user = False
         self.org_admin_scopes = False
         self.org_member = False
-        self.secrets: list[Secret] = []
-        self.runners: list[Runner] = []
+        self.secrets: List[Secret] = []
+        self.runners: List[Runner] = []
         self.sso_enabled = False
+        self.forking_options = {}
 
         self.limited_data = limited_data
 
-        self.public_repos = []
-        self.private_repos = []
+        self.public_repos: List[Repository] = []
+        self.private_repos: List[Repository] = []
+        self.internal_repos: List[Repository] = []
 
         self.name = org_data['login']
 
@@ -44,53 +41,32 @@ class Organization():
             self.org_admin_user = False
             self.org_member = False
 
-    def set_secrets(self, secrets: list[Secret]):
-        """Set repo-level secrets.
-
-        Args:
-            secrets (list): List of secrets at the organization level.
-        """
+    def set_secrets(self, secrets: List[Secret]):
+        """Set org-level secrets.\n\n        Args:\n            secrets (List[Secret]): List of secrets at the organization level.\n        """
         self.secrets = secrets
 
-    def set_repository(self, repo: Repository):
-        """Add a single repository to the organization.
-
-        Args:
-            repo (Repository): Single repository object.
-        """
-        if repo.is_private():
-            self.private_repos.append(repo)
-        else:
-            self.public_repos.append(repo)
-
-    def set_public_repos(self, repos: list[Repository]):
-        """List of public repos for the org.
-
-        Args:
-            repos (List[Repository]): List of Repository wrapper objects.
-        """
+    def set_public_repos(self, repos: List[Repository]):
+        """List of public repos for the org.\n\n        Args:\n            repos (List[Repository]): List of Repository wrapper objects.\n        """
         self.public_repos = repos
 
-    def set_private_repos(self, repos: list[Repository]):
-        """List of private repos for the org.
-
-        Args:
-            repos (List[Repository]): List of Repository wrapper objects.
-        """
+    def set_private_repos(self, repos: List[Repository]):
+        """List of private repos for the org.\n\n        Args:\n            repos (List[Repository]): List of Repository wrapper objects.\n        """
         self.private_repos = repos
 
-    def set_runners(self, runners: list[Runner]):
-        """Set a list of runners that the organization can access.
+    def set_internal_repos(self, repos: List[Repository]):
+        """List of internal repos for the org.\n\n        Args:\n            repos (List[Repository]): List of Repository wrapper objects.\n        """
+        self.internal_repos = repos
 
-        Args:
-            runners (List[Runner]): List of runners that are attached to the
-            organization.
-        """
+    def set_runners(self, runners: List[Runner]):
+        """Set a list of runners that the organization can access.\n\n        Args:\n            runners (List[Runner]): List of runners that are attached to the\n            organization.\n        """
         self.runners = runners
 
+    def set_forking_options(self, options: dict):
+        """Set forking options for the organization.\n\n        Args:\n            options (dict): Dictionary containing forking options.\n        """
+        self.forking_options = options
+
     def toJSON(self):
-        """Converts the repository to a Gato JSON representation.
-        """
+        """Converts the organization to a Gato JSON representation.\n        """
         if self.limited_data:
             representation = {
                 "name": self.name
@@ -103,10 +79,10 @@ class Organization():
                 "org_runners": [runner.toJSON() for runner in self.runners],
                 "org_secrets": [secret.toJSON() for secret in self.secrets],
                 "sso_access": self.sso_enabled,
-                "public_repos":
-                    [repository.toJSON() for repository in self.public_repos],
-                "private_repos":
-                    [repository.toJSON() for repository in self.private_repos]
+                "public_repos": [repository.toJSON() for repository in self.public_repos],
+                "private_repos": [repository.toJSON() for repository in self.private_repos],
+                "internal_repos": [repository.toJSON() for repository in self.internal_repos],
+                "forking_options": self.forking_options
             }
 
         return representation
